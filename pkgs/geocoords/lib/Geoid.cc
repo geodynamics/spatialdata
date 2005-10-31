0f00cb92c512fa94a3455b7ca3e4e3d81685c080
Brad Aagaard <baagaard@usgs.gov>
2005-10-31 16:05:13 +0000

    Fixed setting endian type for reading Geoid binary data.

--- pkgs/geocoords/lib/Geoid.cc
+++ pkgs/geocoords/lib/Geoid.cc
@@ -29,6 +29,25 @@
 #define FIREWALL assert
 #endif
 
+#if defined(__BYTE_ORDER)
+#if __BYTE_ORDER == __BIG_ENDIAN || __BYTE_ORDER == _BIG_ENDIAN
+#define NATIVE_BIG_ENDIAN
+#elif __BYTE_ORDER == _LITTLE_ENDIAN || __BYTE_ORDER == __LITTLE_ENDIAN
+#define NATIVE_LITTLE_ENDIAN
+#else
+#error Unable to determine endian type
+#endif // BYTE_ORDER cases
+#else // __BYTE_ORDER not defined
+#if defined(BIG_ENDIAN) || defined(_BIG_ENDIAN) || defined(__BIG_ENDIAN)
+#define NATIVE_BIG_ENDIAN
+#elif defined(LITTLE_ENDIAN) || defined(_LITTLE_ENDIAN) || defined(__LITTLE_ENDIAN)
+#define NATIVE_LITTLE_ENDIAN
+#else
+#error Unable to determine endian type
+#endif // ENDIAN cases
+#endif // __BYTE_ORDER defined
+
+
 #define MAKEDATAFILE(file) #file
 #define SETDATAFILE(dir,file) MAKEDATAFILE(dir/file)
 
@@ -231,7 +250,7 @@ void
 spatialdata::geocoords::Geoid::_endianBigToNative(int16_t** ppVals,
 						  const int numVals)
 { // _endianBigToNative
-#if 0
+#if defined(NATIVE_LITTLE_ENDIAN)
   for (int i=0; i < numVals; ++i) {
     char* buf = (char*) (*ppVals + i);
     char tmp = buf[1];
