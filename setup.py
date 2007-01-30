fea95ef1262abb6ba3ca1413ac366b600d5d8ec4
Leif Strand <leif@geodynamics.org>
2007-01-30 23:01:54 +0000

    Transitioned spatialdata to the egg-based "CIG Edition" of Pythia. Also fixed build errors relating to .pyxe files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from archimedes import use_merlin
+use_merlin()
+
+from merlin import setup, find_packages
+
+setup(
+    
+    name = 'spatialdata', 
+    version = '0.3.0',
+
+    zip_safe = False,
+    packages = find_packages(),
+    
+    install_requires = [
+    'pythia[mpi] >= 0.8.1.0, < 0.8.2a',
+    ],
+
+    author = 'Brad T. Aagaard',
+    author_email = 'baagaard@usgs.gov',
+    description = """""",
+    license = 'other',
+    url = 'http://www.geodynamics.org/',
+
+)
