3535d90d15f707b9955b9884db9e2cd94c9c4c14
Brad Aagaard <baagaard@usgs.gov>
2008-07-03 22:11:08 +0000

    Old update. Increment version number.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from merlin import setup, find_packages
 setup(
     
     name = 'spatialdata', 
-    version = '0.3.0',
+    version = '0.3.1',
 
     zip_safe = False,
     packages = find_packages(),
