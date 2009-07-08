ca5c75dabd0660b74ea986f53b53b7d1e9e33949
Brad Aagaard <baagaard@usgs.gov>
2009-07-08 20:12:41 +0000

    Incremented version number (added --enable-swig).

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from merlin import setup, find_packages
 setup(
     
     name = 'spatialdata', 
-    version = '0.5.1',
+    version = '0.5.2',
 
     zip_safe = False,
     packages = find_packages(),
