e4d30fc51dcbc13f5ffaf4fb1084ec2e779c4ea8
Brad Aagaard <baagaard@usgs.gov>
2011-11-09 15:43:36 +0000

    Updated version number and list of changes.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from merlin import setup, find_packages
 setup(
     
     name = 'spatialdata', 
-    version = '1.7.1',
+    version = '1.8.0',
 
     zip_safe = False,
     packages = find_packages(),
