8dfc79fafe8318950c6db4f0f49f54addbe1e29c
Brad Aagaard <baagaard@usgs.gov>
2013-09-16 14:35:53 -0700

    Removed Python eggs.
    Switch to generic distutils from pyre/merlin version with eggs. Check
    for Pyre modules in configure.

--- setup.py
+++ setup.py
@@ -5,17 +5,14 @@
 # This code was developed as part of the Computational Infrastructure
 # for Geodynamics (http://geodynamics.org).
 #
-# Copyright (c) 2010-2012 University of California, Davis
+# Copyright (c) 2010-2013 University of California, Davis
 #
 # See COPYING for license information.
 #
 # ----------------------------------------------------------------------
 #
 
-from archimedes import use_merlin
-use_merlin()
-
-from merlin import setup, find_packages
+from distutils.core import setup
 
 setup(
     
@@ -23,16 +20,11 @@ setup(
     version = '1.9.2',
 
     zip_safe = False,
-    packages = find_packages(),
     
-    install_requires = [
-    'pythia[mpi] >= 0.8.1.4, < 0.8.2a',
-    ],
-
     author = 'Brad T. Aagaard',
     author_email = 'baagaard@usgs.gov',
-    description = """C++/Python module for specifying variations in fields over a domain (typically parameters for boundary conditions or physical properties over a simulation domain. The module also supports conversions between coordinate systems, using Proj.4 for conversions between geographic coordinate systems.""",
+    description = """C++/Python module for specifying variations in fields over a domain (typically parameters for boundary conditions or physical properties over a simulation domain). The module also supports conversions between coordinate systems, using Proj.4 for conversions between geographic coordinate systems.""",
     license = 'MIT license',
-    url = 'http://www.geodynamics.org/',
+    url = 'http://www.geodynamics.org/software/pylith',
 
 )
