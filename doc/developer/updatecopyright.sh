080af3d9698a1b842d7d2a27ee92e7985cd4ab8b
Brad Aagaard <baagaard@usgs.gov>
2016-06-14 12:03:42 -0700

    Updated copyright.

--- doc/developer/updatecopyright.sh
+++ doc/developer/updatecopyright.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-for suffix in .cc .icc .hh .h .am .py .i .odb .dat .in; do
-  for f in `find . -name "*$suffix"` configure.ac aclocal.m4 CHANGES README COPYING; do
-    sed -e "s/Copyright (c) 2010 University of California, Davis/Copyright (c) 2010-2015 University of California, Davis/g" $f > tmp && mv -f tmp $f
-    sed -e "s/Copyright (c) 2010-2014 University of California, Davis/Copyright (c) 2010-2015 University of California, Davis/g" $f > tmp && mv -f tmp $f
+for suffix in .cc .icc .hh .h .am .py .i .odb .dat .in *.ac; do
+  for f in `find . -name "*$suffix"` aclocal.m4 CHANGES README COPYING; do
+    sed -e "s/Copyright (c) 2010-2015 University of California, Davis/Copyright (c) 2010-2016 University of California, Davis/g" $f > tmp && mv -f tmp $f
   done
 done
