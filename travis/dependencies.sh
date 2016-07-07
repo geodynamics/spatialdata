cc1426551eb79392990e2dd14e01f0c516eb7ca9
Brad Aagaard <baagaard@usgs.gov>
2016-07-07 09:24:11 -0700

    Force install, just to be sure.

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -11,7 +11,7 @@ TARBALL=spatialdata_deps-${TRAVIS_OS_NAME}.tgz
 REPO=http://geoweb.cse.ucdavis.edu/~buildbot/deps
 
 if [ $action == "download" ]; then
-  curl -O ${REPO}/${TARBALL} && tar -zxvf ${TARBALL}
+  curl -O ${REPO}/${TARBALL} && tar -zhxvf ${TARBALL}
 
 elif [ $action == "build" ]; then
 
@@ -19,7 +19,7 @@ git clone --depth 1 --recursive https://github.com/geodynamics/pylith_installer.
 
 pushd pylith_installer && \
   autoreconf --install --verbose && \
-  ./configure --prefix=${TOP_DIR}/dist --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --with-make-threads=${MAKE_THREADS} && \
+  ./configure --prefix=${TOP_DIR}/dist --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} && \
   . ./setup.sh && \
   make deps && \
   popd && \
