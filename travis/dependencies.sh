fdd0cafb3ac7f685d34ffd250f60d05aee0cbfb3
Brad Aagaard <baagaard@usgs.gov>
2016-07-07 12:11:10 -0700

    Try to use caching built-in to Travis.

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-action="download"
+action="build"
 if [ $# == 1 ]; then
   action=$1
 fi
 
 MAKE_THREADS=2
 TOP_DIR=`pwd`
-TARBALL=spatialdata_deps-${TRAVIS_OS_NAME}.tgz
-REPO=http://geoweb.cse.ucdavis.edu/~buildbot/deps
+#TARBALL=spatialdata_deps-${TRAVIS_OS_NAME}.tgz
+#REPO=http://geoweb.cse.ucdavis.edu/~buildbot/deps
 
 if [ $action == "download" ]; then
   curl -O ${REPO}/${TARBALL} && tar -zhxvf ${TARBALL}
@@ -19,11 +19,12 @@ git clone --depth 1 --recursive https://github.com/geodynamics/pylith_installer.
 
 pushd pylith_installer && \
   autoreconf --install --verbose && \
-  ./configure --prefix=${TOP_DIR}/dist --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} && \
+  ./configure --prefix=${TOP_DIR}/dist/deps --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --enable-force-install --with-make-threads=${MAKE_THREADS} && \
   . ./setup.sh && \
   make deps && \
-  popd && \
-  tar -zcvf $TARBALL dist && curl -T ${TARBALL} ftp://ftpext.usgs.gov/from_pub/wr/
+  popd
+
+ # tar -zcvf $TARBALL dist && curl -T ${TARBALL} ftp://ftpext.usgs.gov/from_pub/wr/
 fi
 
 # End of file
