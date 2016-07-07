22c5f8528553310aa1704c7e1cabcf9dad358305
Brad Aagaard <baagaard@usgs.gov>
2016-07-07 08:55:09 -0700

    Default is no fortran.

--- travis/dependencies.sh
+++ travis/dependencies.sh
@@ -5,7 +5,7 @@ if [ $# == 1 ]; then
   action=$1
 fi
 
-MAKE_THREADS=16
+MAKE_THREADS=2
 TOP_DIR=`pwd`
 TARBALL=spatialdata_deps-${TRAVIS_OS_NAME}.tgz
 REPO=http://geoweb.cse.ucdavis.edu/~buildbot/deps
@@ -19,7 +19,7 @@ git clone --depth 1 --recursive https://github.com/geodynamics/pylith_installer.
 
 pushd pylith_installer && \
   autoreconf --install --verbose && \
-  ./configure --prefix=${TOP_DIR}/dist --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --enable-force-install --with-make-threads=${MAKE_THREADS} && \
+  ./configure --prefix=${TOP_DIR}/dist --enable-mpi=mpich --enable-cppunit --enable-numpy --enable-proj4 --enable-hdf5 --enable-netcdfpy --enable-cmake --enable-setuptools --enable-nemesis --enable-fiat --enable-pcre --enable-swig --with-fortran=no --with-make-threads=${MAKE_THREADS} && \
   . ./setup.sh && \
   make deps && \
   popd && \
