47405bf93f0e2617efc6db9c53001090b57a20c2
Brad Aagaard <baagaard@usgs.gov>
2020-05-04 15:03:40 -0600

    Setup configure.ac for test coverage on/off and different python implementations.

--- ci-config/run_test_coverage.sh
+++ ci-config/run_test_coverage.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# Run tests to generate coverage information. Upload test coverage data
+# Run tests to generate coverage information. Upload test coverage data.
 # Must run codeocov script in top-level source directory.
 
 make -j$(nproc) install
 if [ $? != 0 ]; then exit 1; fi
 
-make -j$(nproc) check VERBOSE=1
+
+make -j$(nproc) check -C tests/libtests VERBOSE=1
 if [ $? != 0 ]; then exit 1; fi
 
-LCOV=`which lcov`
-if [ "x" != "x$LCOV" ]; then
-  make coverage-libtests
+make coverage-libtests
+if [ $? != 0 ]; then exit 1; fi
 
+if [ -r coverage-libtests.info ]; then
   pushd ../../src/spatialdata && \
       bash <(curl -s https://codecov.io/bash) -X gcov -f ../../build/spatialdata/coverage-libtests.info -F libtests -y ci-config/codecov.yml \
 	  || echo "Codecov did not collect coverage reports." && \
       popd
-else
-    echo "lcov not found. Skipping C++ test coverage."
 fi
 
-PYCOV=`which coverage`
-if [ "x" != "x$PYCOV" ]; then
-  make coverage-pytests
+make -j$(nproc) check -C tests/pytests VERBOSE=1
+if [ $? != 0 ]; then exit 1; fi
 
+make coverage-pytests
+if [ $? != 0 ]; then exit 1; fi
+
+if [ -r coverage-pytests.xml ]; then
   pushd ../../src/spatialdata && \
       bash <(curl -s https://codecov.io/bash) -X gcov -f ../../build/spatialdata/coverage-pytests.xml -F pytests -y ci-config/codecov.yml \
 	  || echo "Codecov did not collect coverage reports." && \
       popd
-else
-    echo "coverage not found. Skipping Pyhton test coverage."
 fi
 
 exit 0
